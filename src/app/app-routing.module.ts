d0d1cf554577fccfbc2bb114eba63136cba41a40
Osman-Ozturk <ztrkosmn09@gmail.com>
2023-04-28 23:02:07 +0200

    shared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './authentication/auth/auth.component';
 import { CategoryCreateComponent } from './categories/category-create/category-create.component';
-import { HomeComponent } from './home/home.component';
+import { AdminGuard } from './authentication/guards/admin.guard';
+import { HomeComponent } from './shared/home/home.component';
 import { ProductCreateComponent } from './products/product-create/product-create.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductComponent } from './products/product/product.component';
-import { AdminGuard } from './authentication/guards/admin-guard';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'products/create', component: ProductCreateComponent,canActivate:[AdminGuard]},
-  { path: 'categories/create', component: CategoryCreateComponent,canActivate:[AdminGuard]},
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'products/create', component: ProductCreateComponent, canActivate: [AdminGuard]},
+  { path: 'categories/create', component: CategoryCreateComponent, canActivate: [AdminGuard]},
   { path: 'products', component: ProductListComponent },
   { path: 'products/:productId', component: ProductComponent },
   { path: 'products/category/:categoryId', component: ProductListComponent},
-  { path: 'auth', component: AuthComponent }
+  { path: 'auth', component: AuthComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './shared/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { ProductsModule } from './products/products.module';
-import { CkeditorModule } from './ckeditor.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    CkeditorModule,
     ProductsModule,
     CategoriesModule,
-    AuthenticationModule
+    AuthenticationModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
